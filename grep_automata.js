08c0f3388cd7f1eb4b2a86cd843cc407471c3e88
Arturo Avilés <artcastell@gmail.com>
2015-10-17 18:01:52 -0500

    Added functionality to operation(automata,input) enabling it to search a word in a text and printing lines just as grep does

--- grep_automata.js
+++ grep_automata.js
@@ -1,7 +1,17 @@
 /****************************************** Credits ******************************************
-Arturo Avilés Castellanos
-Yael Araizaga Yerid Gracia
+Arturo Avilés Castellanos A01372760
+Yael Araizaga Yerid Gracia A01166495
+
+Thanks to Dr. Salvador E. Venegas-Andraca
 **********************************************************************************************/
+
+
+/****************************************** Functions ****************************************/
+// 1. Create Automata from WORD to be searched (Array).
+// 2. Create Input form TEXT to be searched (Array).
+// 3. Transition function that allows to search the WORD in the TEXT
+// 4. isASCII is an extra feature to see if the chareacters of the WORD to be searched
+//    is insde the ASCII list. Erase it if you want full search on haha. 
 
 // Create automata
 function createAutomata(word){
@@ -36,13 +46,54 @@ function input(text){
 }
 
 function operation(automata, input){
+	var text = input;
+	var word = automata.transitions;
 
+	// Array del texto
+	var chars = text.split(''); // This will allow to read char by char
+	var lines = text.split('\n');
+	
+	// Array de la palabra
+	var word = word.split(''); // This will allow to compare it
+	
+	// Counters
+	var linesCounter = 1; // It will increase every time it detects a '\n'
+	var checkCounter = 0; // It will increase every time it detects a match
+	var flag = false; // This flag will turn true if it finds at least a match in a line
+	
+	// Read char by char in the TEXT
+	for (var i = 0; i <= chars.length; i++){  
+		if(chars[i]=='\n' || i == chars.length){
+			if(flag){
+				document.write("<br>"+linesCounter+" "+lines[linesCounter-1]);
+				flag = false;
+			}  
+			linesCounter++;
+		} 
+		// Read char by char in the WORD
+		for(var j = 0; j < word.length; j++){
+			if(chars[i]!=word[j]){
+				checkCounter = 0;
+				break;
+			}
+			while(chars[i]==word[j]){
+				checkCounter++;
+				i++;
+				j++;
+			}
+			if(checkCounter==word.length){
+				flag = true;
+			}
+		}
+	}
 }
 
 // Verify if the word to search is in the ASCII Symbols // Extra function
 function isASCII(str) {
     return /^[\x00-\x7F]*$/.test(str);
 }
+
+/**********************************************************************************************/
 
 // Start of the Program
 console.log('The program started');
@@ -58,12 +109,8 @@ do{
 // Create an automata that accepts searchWord
 var automata = createAutomata(searchWord);
 
-// Make the textArea an Array of chars
-var input = input(textArea);
-
 // Make it work!
-operation(automata,input);          //
-
+operation(automata,textArea);          //
 
 // End of program
 console.log('The program ended correctly');
